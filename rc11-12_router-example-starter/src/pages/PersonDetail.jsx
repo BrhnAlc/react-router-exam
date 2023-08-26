61109d2d843fe89186a3d9ad2fa9fb9f50022f60
burhan <alacaburhan122016@gmail.com>
2023-08-26 23:49:15 +0300

    navigate

--- rc11-12_router-example-starter/src/pages/PersonDetail.jsx
+++ rc11-12_router-example-starter/src/pages/PersonDetail.jsx
@@ -7,7 +7,22 @@ const PersonDetail = () => {
 
   console.log(person);
   return (
-    <div className="container text-center">PersonDetail</div>
+    <div className="container text-center">
+      
+      <h3>
+        {person?.first_name } {person?.last_name}
+      </h3>
+      <img className="rounded" src={person?.avatar} alt="" />
+      <p>{person?.email}</p>
+      <div>
+        <button onClick={()=>navigate("/")} className="btn btn-success">
+          Go Home
+        </button>
+        <button onClick={()=>navigate(-1)} className="btn btn-warning">
+          Go Back
+        </button>
+      </div>
+      </div>
   )
 }
 
